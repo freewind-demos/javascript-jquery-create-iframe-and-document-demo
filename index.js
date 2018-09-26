99e18ba5cfdc9d7d3256a1d5f2e082f045b27ff7
freewind <nowindlee@gmail.com>
2018-09-26 20:05:26 +0800

    improve

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(function () {
   $('body').append('<iframe></iframe>');
   const frameWindow = $('iframe')[0].contentWindow;
-  frameWindow.document.body = frameWindow.document.createElement('body');
   $(frameWindow.document.body).append('<h1>Text created by JavaScript</h1>');
 });
